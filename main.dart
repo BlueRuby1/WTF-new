207d6f7973ef49d785ac3f3618ba21e172af166f
BlueRuby1 <ayomiposialadesuyi@gmail.com>
2025-10-04 10:18:11 +0100

    init commit

---
+++ main.dart
@@ -0,0 +1,9 @@
+void main() {
+  var name = 'Anne';
+  name = 'Nkechi';
+  print(name);
+
+  final reciever = 'Anne';
+
+  print(reciever);
+}
---
+++ variables.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = "Cynthia";
+  var reciever = "Nkechi";
+
+  print(name + " sends a message to " + reciever);
+  print("$name said no to $reciever");
+}
