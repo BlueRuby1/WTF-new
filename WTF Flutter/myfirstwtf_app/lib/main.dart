015022ee60b48425ade6327f18edbe95843bc2e8
Ayomiposi <ayomiposialadesuyi@gmail.com>
2025-10-27 13:52:12 +0100

    I created a pages folder

--- WTF Flutter/myfirstwtf_app/lib/main.dart
+++ WTF Flutter/myfirstwtf_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:myfirstwtf_app/bottom_navigation.dart';
-import 'package:myfirstwtf_app/home_page.dart';
-import 'package:myfirstwtf_app/onboarding_page.dart';
+import 'package:myfirstwtf_app/pages/bottom_navigation.dart';
+// import 'package:myfirstwtf_app/home_page.dart';
+import 'package:myfirstwtf_app/pages/onboarding_page.dart';
+
 
 void main() {
   runApp(const MyApp());
